8a55b6d83155f7e530e9cb773b914a4a91e73a61
Ben Keith <ben@benlk.com>
2017-08-26 16:56:43 -0400

    remove some analytics

--- www/js/analytics.js
+++ www/js/analytics.js
@@ -147,58 +147,6 @@ var ANALYTICS = (function () {
         setupDotOrgAnalytics();
      }
 
-
-    /*
-     * Comscore
-     */
-    var setupComscore = function() {
-        _comscore.push({ c1: "2", c2: "17691522" });
-
-        (function() {
-            var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
-            s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
-            el.parentNode.insertBefore(s, el);
-        })();
-    }
-
-    /*
-     * Nielson
-     */
-    var setupNielson = function() {
-        (function () {
-            var d = new Image(1, 1);
-            d.onerror = d.onload = function () { d.onerror = d.onload = null; };
-            d.src = ["//secure-us.imrworldwide.com/cgi-bin/m?ci=us-803244h&cg=0&cc=1&si=", escape(window.location.href), "&rp=", escape(document.referrer), "&ts=compact&rnd=", (new Date()).getTime()].join('');
-        })();
-    }
-
-    /*
-     * Chartbeat
-     */
-    var setupChartbeat = function() {
-        /** CONFIGURATION START **/
-        _sf_async_config.uid = 18888;
-        _sf_async_config.domain = "npr.org";
-        /** CONFIGURATION END **/
-        (function(){
-            function loadChartbeat() {
-                window._sf_endpt=(new Date()).getTime();
-                var e = document.createElement("script");
-                e.setAttribute("language", "javascript");
-                e.setAttribute("type", "text/javascript");
-                e.setAttribute("src",
-                    (("https:" == document.location.protocol) ?
-                     "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" :
-                     "http://static.chartbeat.com/") +
-                    "js/chartbeat.js");
-                document.body.appendChild(e);
-            }
-            var oldonload = window.onload;
-            window.onload = (typeof window.onload != "function") ?
-                loadChartbeat : function() { oldonload(); loadChartbeat(); };
-        })();
-    }
-
     /*
      * Event tracking.
      */
